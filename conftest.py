929ad389d659e7575df7932fcce89f3e34858587
v.denisov <v.denisov@postgrespro.ru>
2018-05-14 15:27:28 +0300

    test

--- conftest.py
+++ conftest.py
@@ -3,11 +3,12 @@ __author__ = 'vden'
 import pytest
 import json
 import os.path
-import importlib
-import jsonpickle
+
 from fixture.application import Application
+from fixture.db import Dbfixture
+
+
 from model.mbt_host import Mbt_hosts
-#from fixture.orm import ORMFixture
 
 fixture = None
 target = None
@@ -23,7 +24,8 @@ def load_mbt_hosts():
             mbt_hosts_p = mbt_hosts_json["mbt_hosts"]
         for mbt_host in mbt_hosts_p:
             mbt_hosts.append(Mbt_hosts(host=mbt_host["host"], user=mbt_host["user"], password=mbt_host["password"],
-                          database=mbt_host["database"], port=mbt_host["port"], type=mbt_host['type']))
+                          database=mbt_host["database"], port=mbt_host["port"], write=mbt_host['write'],
+                                       read=mbt_host['read']))
 
     return mbt_hosts
 
@@ -36,53 +38,18 @@ def app(request):
     if fixture is None :
         fixture = Application(mbt_hosts)
 
+
+
     return fixture
-
-
-"""
-@pytest.fixture(scope='session')
-def orm(request):
-    orm_config = load_config(request.config.getoption("--target"))["db"]
-    ormfixture = ORMFixture(host=orm_config["host"], name=orm_config["name"], user=orm_config["user"], password=orm_config["password"])
-    return ormfixture
-
-
-@pytest.fixture(scope='session', autouse=True)
-def stop(request):
-    def fin():
-        fixture.session.ensure_logout()
-        fixture.destroy()
-    request.addfinalizer(fin)
-    return fixture
-
 
 @pytest.fixture
-def check_ui(request):
-    return request.config.getoption("--check_ui")
+def db(request):
+    pass
+    """
 
-def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--target", action="store", default="target.json")
-    parser.addoption("--check_ui", action="store_true")
-
-
-
-def pytest_generate_tests(metafunc):
-    for fixture in metafunc.fixturenames:
-        if fixture.startswith("data_"):
-            testdata = load_from_module(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-        elif fixture.startswith("json_"):
-            testdata = load_from_json(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-
-
-
-def load_from_module(module):
-    return importlib.import_module("data.%s" % module).testdata
-
-
-def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-        return jsonpickle.decode(f.read())
-"""
+    dbfixture = Dbfixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+    """
--- model/mbt_host.py
+++ model/mbt_host.py
@@ -2,14 +2,16 @@ __author__ = 'vden'
 
 class Mbt_hosts:
 
-    def __init__(self, host=None, user=None, password=None, database=None, port=None, type=None):
+    def __init__(self, host=None, user=None, password=None, database=None, port=None, write=None, read=None):
         self.host = host
         self.user= user
         self.password= password
         self.database= database
         self.port= port
-        self.type= type
+        self.write= write
+        self.read= read
 
 
     def __repr__(self):
-        return "%s,%s,%s,%s,%s,%s" % (self.host, self.user, self.password, self.database, self.port, self.type)
+        return "%s,%s,%s,%s,%s,%s,%s" % (self.host, self.user, self.password, self.database, self.port,
+                                         self.write, self.read)
