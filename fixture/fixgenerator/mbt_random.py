331610dfd27f507c5c07e4c00e3302cb26cbe5e3
v.denisov <v.denisov@postgrespro.ru>
2018-11-14 17:10:03 +0300

    test

--- fixture/fixgenerator/mbt_random.py
+++ fixture/fixgenerator/mbt_random.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'vden'
 
+from model.table_mbt_random import Table_mbt_random
 
 
 class MbtRandomHelper():
@@ -9,7 +10,21 @@ class MbtRandomHelper():
         self.generator = generator
 
     def random_generator(self):
-        pass
+        fibonacci_list = self.generator.fibonacci.numbers_list()
+        p_points_list = self.generator.p_points.g_points(coumt_points=1000)
+
+        mbt_random_list=[]
+        f_l = 0
+        for p in p_points_list:
+            mbt_random_list.append(Table_mbt_random(fib_number=fibonacci_list[f_l],
+                                                    p_point_x=p.p_point_x, p_point_y=p.p_point_y))
+            if f_l==91:
+                f_l = 0
+            else:
+                f_l += 1
+
+        return mbt_random_list
+
 
 
 
--- model/table_mbt_random.py
+++ model/table_mbt_random.py
@@ -2,16 +2,15 @@ __author__ = 'vden'
 
 class Table_mbt_random:
 
-    def __init__(self, id=None, fib_number=None, p_point_x=None, p_point_y=None):
-        self.id = id
+    def __init__(self, fib_number=None, p_point_x=None, p_point_y=None):
         self.fib_number = fib_number
         self.p_point_x = p_point_x
         self.p_point_y = p_point_y
 
 
     def __repr__(self):
-        return "%s,%s,%s,%" % (self.id,self.fib_number, self.p_point_x, self.p_point_y)
+        return "%s,%s,%s" % (self.fib_number, self.p_point_x, self.p_point_y)
 
     def __eq__(self, other):
-        return (self.id==other.id and self.fib_number==other.fib_number and self.p_point_x==other.p_point_x
+        return (self.fib_number==other.fib_number and self.p_point_x==other.p_point_x
                 and self.p_point_y==other.p_point_y )
