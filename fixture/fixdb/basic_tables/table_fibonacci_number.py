bad6d921332a236f11a834ca537f0f9706f845d1
v.denisov <v.denisov@postgrespro.ru>
2018-12-10 16:43:38 +0300

    test

--- fixture/fixdb/basic_tables/table_fibonacci_number.py
+++ fixture/fixdb/basic_tables/table_fibonacci_number.py
@@ -4,7 +4,7 @@ import pytest
 
 
 count_table_fibonacci_number = 0
-table_fibonacci_number = None
+table_fibonacci_number_name = None
 
 class Table_fibonacci_numberHelper():
 
@@ -15,18 +15,18 @@ class Table_fibonacci_numberHelper():
 
         tablename='fibonacci_number_'+self.db.app.mbt_conn.test_uuid
         self.db.fibonacci_number.create_table(table_name=tablename)
+        return tablename
 
 
 
     @pytest.allure.step('insert in table "fibonacci_number"')
     def insert(self, list_table_fibonacci_numbers=None, commit=True):
         global count_table_fibonacci_number
-        global table_fibonacci_number
-        if table_fibonacci_number is None:
-            self.create_table()
-            table_fibonacci_number = True
+        global table_fibonacci_number_name
+        if table_fibonacci_number_name is None:
+            table_fibonacci_number_name = self.create_table()
 
-        tablename = 'fibonacci_number_' + self.db.app.mbt_conn.test_uuid
+        tablename = table_fibonacci_number_name
         self.db.fibonacci_number.insert(list_table_fibonacci_numbers=list_table_fibonacci_numbers,
                                         commit=commit, table_name=tablename)
         if commit==True:
@@ -35,14 +35,16 @@ class Table_fibonacci_numberHelper():
     @pytest.allure.step('check count')
     def check_count(self):
         global count_table_fibonacci_number
-        table_name = ("fibonacci_number_{test_uuid}").format(test_uuid=self.db.app.mbt_conn.test_uuid)
-        c_count = self.db.table_check.check_count(count_rows_table=count_table_fibonacci_number, table_name=table_name)
+        global table_fibonacci_number_name
+        tablename = table_fibonacci_number_name
+        c_count = self.db.table_check.check_count(count_rows_table=count_table_fibonacci_number, table_name=tablename)
         return  c_count
 
 
     @pytest.allure.step('check records')
     def check_records(self):
-        tablename = 'fibonacci_number_' + self.db.app.mbt_conn.test_uuid
+        global table_fibonacci_number_name
+        tablename = table_fibonacci_number_name
         check_records_c = self.db.fibonacci_number.check_records(table_name=tablename)
         return check_records_c
 
@@ -56,7 +58,8 @@ class Table_fibonacci_numberHelper():
         if c_limit>10000:
             c_limit=10000
 
-        tablename = 'fibonacci_number_' + self.db.app.mbt_conn.test_uuid
+        global table_fibonacci_number_name
+        tablename = table_fibonacci_number_name
         self.db.fibonacci_number.update_id_random(c_limit=c_limit, table_name=tablename, commit=commit)
 
 
@@ -69,7 +72,8 @@ class Table_fibonacci_numberHelper():
         global count_table_fibonacci_number
         c_limit = count_table_fibonacci_number // 50 + 1
 
-        tablename = 'fibonacci_number_' + self.db.app.mbt_conn.test_uuid
+        global table_fibonacci_number_name
+        tablename = table_fibonacci_number_name
         self.db.fibonacci_number.delete_2_percent_of_rows(c_limit=c_limit, table_name=tablename, commit=commit)
 
         if commit == True:
