210af391009e4a94dc8ef040c1e19780b1c0974c
v.denisov <v.denisov@postgrespro.ru>
2019-01-14 16:42:13 +0300

    test

---
+++ test_group/test_extension/test_autonomous_transactions/test_table_fn_autonomous_transactions_in_one_table/test_table_fn_autonomous_transactions_in_one_table_multi_insert_commit.py
@@ -0,0 +1,26 @@
+__author__ = 'vden'
+import pytest
+from model.basic_tables.table_fibonacci_number import Table_fibonacci_number
+
+@pytest.mark.pg_pathman
+@pytest.allure.step('test_table_fn_autonomous_transactions_in_one_table_insert_commit')
+def test_table_fn_autonomous_transactions_in_one_table_insert_commit(db, generator):
+    print("\n\ntest_table_fn_autonomous_transactions_in_one_table_insert_commit \n\n")
+
+    fib = generator.fibonacci.numbers_list()
+
+    cycle_factor=db.app.mbt_conn.cycle_factor
+    for x in range(10*cycle_factor):
+
+        for x in range(100):
+
+            list_table_fn_autonomous_transactions_in_one_table = []
+            for i_fib in fib:
+                list_table_fn_autonomous_transactions_in_one_table.append(Table_fibonacci_number(id=0,fib_number=i_fib))
+            with pytest.allure.step('insert in the test_table_fn_autonomous_transactions_in_one_table %s'
+                                            % fib):
+                db.fn_autonomous_transactions_in_one_table.autonomous_transactions_insert(
+                    list_table_fibonacci_numbers=list_table_fn_autonomous_transactions_in_one_table)
+
+    assert(db.fn_autonomous_transactions_in_one_table.check_count())
+    assert (db.fn_autonomous_transactions_in_one_table.check_records())
