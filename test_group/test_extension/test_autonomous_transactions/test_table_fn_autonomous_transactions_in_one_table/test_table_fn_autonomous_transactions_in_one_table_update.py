9a3c41df4d3fe341636c22e98fec5da9931cf21d
v.denisov <v.denisov@postgrespro.ru>
2019-01-15 14:50:04 +0300

    test

---
+++ test_group/test_extension/test_autonomous_transactions/test_table_fn_autonomous_transactions_in_one_table/test_table_fn_autonomous_transactions_in_one_table_update.py
@@ -0,0 +1,36 @@
+__author__ = 'vden'
+import pytest
+from model.basic_tables.table_fibonacci_number import Table_fibonacci_number
+
+@pytest.mark.pg_pathman
+@pytest.allure.step('test_table_fn_autonomous_transactions_in_one_table_update')
+def test_table_fn_autonomous_transactions_in_one_table_update(db, generator):
+    print("\n\ntest_table_fn_autonomous_transactions_in_one_table_update \n\n")
+
+    fib = generator.fibonacci.numbers_list()
+    list_table_fn_pg_pathman_hash = []
+    for i_fib in fib:
+        list_table_fn_pg_pathman_hash.append(Table_fibonacci_number(fib_number=i_fib))
+    for x in range(20):
+        with pytest.allure.step('insert in the table the Fibonacci number %s' % fib):
+            db.fn_autonomous_transactions_in_one_table.insert(
+                list_table_fibonacci_numbers=list_table_fn_pg_pathman_hash)
+
+
+
+    #cycle_factor = db.app.mbt_conn.cycle_factor
+    for y in range(10):
+
+        count_table_fn_pg_pathman_hash=db.fn_autonomous_transactions_in_one_table.get_count_table_fibonacci_number()
+        # print('count_table_fn_pg_pathman_hash=', count_table_fn_pg_pathman_hash)
+        # print("\n range=", y)
+
+
+
+        with pytest.allure.step('update id more than number'):
+            db.fn_autonomous_transactions_in_one_table.update_id_random()
+
+
+
+    assert(db.fn_autonomous_transactions_in_one_table.check_count())
+    assert (db.fn_autonomous_transactions_in_one_table.check_records())
