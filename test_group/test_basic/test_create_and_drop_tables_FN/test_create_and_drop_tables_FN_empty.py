c0237167ce91086a0294354431c812f88359aed1
v.denisov <v.denisov@postgrespro.ru>
2019-01-31 13:11:38 +0300

    test

---
+++ test_group/test_basic/test_create_and_drop_tables_FN/test_create_and_drop_tables_FN_empty.py
@@ -0,0 +1,15 @@
+__author__ = 'vden'
+
+import pytest
+import allure
+
+
+@pytest.mark.test_basic
+@allure.step('test_table_fibonacci_number_insert_commit')
+def test_create_tables_FN_empty():
+    pass
+
+@pytest.mark.test_basic
+@allure.step('test_table_fibonacci_number_insert_commit')
+def test_drop_tables_FN_empty():
+    pass
