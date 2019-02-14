fb197ff1d56a9d67ad1984cca37097fc506c5c94
v.denisov <v.denisov@postgrespro.ru>
2019-02-14 15:17:26 +0300

    test

--- test_group/test_basic/test_table_points_index_gist/test_table_points_index_gist_update_rollback.py
+++ test_group/test_basic/test_table_points_index_gist/test_table_points_index_gist_update_rollback.py
@@ -9,7 +9,7 @@ def test_table_points_index_gist_update_rollback(db, generator):
 
     #cycle_factor = db.app.mbt_conn.cycle_factor
     for x in range(5):
-        list_points = generator.p_points.g_points(coumt_points=1000)
+        list_points = generator.p_points.g_points(coumt_points=100)
 
         with allure.step('insert in the table the points index gist %s' % list_points):
             db.table_points_index_gist.insert(list_points=list_points)
