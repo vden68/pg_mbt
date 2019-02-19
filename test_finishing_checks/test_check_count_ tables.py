1ec046c18bdbeb49f6598fdd50a6bc77760706a4
v.denisov <v.denisov@postgrespro.ru>
2019-02-19 18:04:05 +0300

    test

--- test_finishing_checks/test_check_count_ tables.py
+++ test_finishing_checks/test_check_count_ tables.py
@@ -6,3 +6,12 @@ def test_check_count_tables(db):
     list_model=db.finishing_checks.get_list_tables()
     print("list_model=",list_model, len(list_model))
 
+    for selected_node in db.app.mbt_hosts_read:
+        if selected_node.node_id==1:
+            continue
+        else:
+            list_node = db.finishing_checks.get_list_tables(selected_node=selected_node)
+            print("selected_node.node_id=",selected_node.node_id, "list_node=", list_node)
+            assert (list_model==list_node)
+
+
