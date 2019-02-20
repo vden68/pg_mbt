e3f0e10193acb235081908b93a3b1e6aa879ff7d
v.denisov <v.denisov@postgrespro.ru>
2019-02-20 14:58:01 +0300

    test

--- model/check_list_tables.py
+++ model/check_list_tables.py
@@ -2,12 +2,13 @@ __author__ = 'vden'
 
 class Check_list_tables:
 
-    def __init__(self, table_name=None):
+    def __init__(self, table_name=None, count_rows=None):
         self.table_name = table_name
+        self.count_rows = count_rows
 
 
     def __repr__(self):
-        return "%s" % (self.table_name,)
+        return "%s,%s" % (self.table_name, self.count_rows)
 
     def __eq__(self, other):
-        return (self.table_name==other.table_name)
+        return (self.table_name==other.table_name, self.count_rows==other.count_rows)
--- test_finishing_checks/test_check_count_ tables.py
+++ test_finishing_checks/test_check_count_ tables.py
@@ -7,11 +7,8 @@ def test_check_count_tables(db):
     print("list_model=",list_model, len(list_model))
 
     for selected_node in db.app.mbt_hosts_read:
-        if selected_node.node_id==1:
-            continue
-        else:
-            list_node = db.finishing_checks.get_list_tables(selected_node=selected_node)
-            print("selected_node.node_id=",selected_node.node_id, "list_node=", list_node)
-            assert (list_model==list_node)
+        list_node = db.finishing_checks.get_list_tables(selected_node=selected_node)
+        print("selected_node.node_id=",selected_node.node_id, "list_node=", list_node)
+        assert (list_model==list_node)
 
 
