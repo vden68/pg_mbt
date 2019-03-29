ad152a281fd9d22c48ee7a39ed7701651a90267b
v.denisov <v.denisov@postgrespro.ru>
2019-03-29 17:32:27 +0300

    test

--- model/check_list_tables.py
+++ model/check_list_tables.py
@@ -2,13 +2,15 @@ __author__ = 'vden'
 
 class Check_list_tables:
 
-    def __init__(self, table_name=None, count_rows=None):
+    def __init__(self, table_name=None, count_rows=None, table_md5=None):
         self.table_name = table_name
         self.count_rows = count_rows
+        self.table_md5 = table_md5
 
 
     def __repr__(self):
-        return "%s,%s" % (self.table_name, self.count_rows)
+        return "%s,%s,%s" % (self.table_name, self.count_rows, self.table_md5)
 
     def __eq__(self, other):
-        return (self.table_name==other.table_name and self.count_rows==other.count_rows)
+        return (self.table_name==other.table_name and self.count_rows==other.count_rows and
+                self.table_md5==other.table_md5)
